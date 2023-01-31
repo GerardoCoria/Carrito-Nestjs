df2b38f0c7697a5fd28416a756071798ea05a7d0
Gerardo <gerardo_coria@solutionbox.com.ar>
2023-01-31 12:12:48 -0300

    agrega local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from "@nestjs/passport";
+
 import { AuthService } from './services/auth.service';
+import { UsersModule } from "./../users/users.module";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from "./../../users/services/users.service";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userServices:UsersService){}
+
+  async validateUser(email:string, password:string){
+    const user = await this.userServices.findOne(email)
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if(user && matchPassword){
+      return user;
+    }
+    return null;
+  }
+}
